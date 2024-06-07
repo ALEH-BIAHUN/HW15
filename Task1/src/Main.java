09178b1d8fd2c181058b65bfc9120031146dd576
Aleh Biahun <stones2875@gmail.com>
2024-06-07 12:56:45 +0200

    Solution of task number 15

--- Task1/src/Main.java
+++ Task1/src/Main.java
@@ -1,9 +1,22 @@
 public class Main {
+
     //todo Напишите код который распечатает букву и ее код для всех букв русского алфавита формат вывода:
     // А-123
     // Б-321...
     // Коды даны для примера и не соответствуют действительности. Учитывайте, что нужно распечатать коды букв в двух регистрах!
+
+    public static final String RUSSIAN_ALPHABET = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
+
     public static void main(String[] args) {
-        //todo код писать тут
+
+    printingTheCodesOfTheRussianAlphabet();
+
+    }
+    public static void printingTheCodesOfTheRussianAlphabet() {
+        for (int i = 0; i < RUSSIAN_ALPHABET.length(); i++) {
+            char ch = RUSSIAN_ALPHABET.charAt(i);
+            int charCode = (int) ch;
+            System.out.print(ch + "-" + charCode + "\n");
+        }
     }
 }
--- Task2/src/Main.java
+++ Task2/src/Main.java
@@ -1,5 +1,36 @@
+import java.util.Scanner;
+
 public class Main {
+    private static final String VALID_STRING = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ -";
+
     public static void main(String[] args) {
-        //todo код писать тут. По желанию(рекомендуется) вынести код в методы.
+
+        inputFio();
+    }
+
+        public static void inputFio () {
+            boolean isValid = true;
+            int spaceCount = 0;
+
+            System.out.println("Введите свое ФИО");
+            String inputFio = new Scanner(System.in).nextLine().trim();
+
+            for (int i = 0; i < inputFio.length(); i++) {
+                if (!VALID_STRING.contains(String.valueOf(inputFio.charAt(i)))) {
+                    isValid = false;
+                    break;
+                }
+                if (inputFio.charAt(i) == ' ') {
+                    spaceCount++;
+                }
+            }
+
+            if (isValid && spaceCount == 2) {
+                System.out.println("Фамилия: " + inputFio.substring(0, inputFio.indexOf(" ")));
+                System.out.println("Имя: " + inputFio.substring(inputFio.indexOf(" "), inputFio.lastIndexOf(" ")));
+                System.out.println("Отчество:" + inputFio.substring(inputFio.lastIndexOf(" "), inputFio.length()));
+            } else {
+                System.out.println("Введенная строка не является ФИО");
+        }
     }
 }
